037fbdb107e19b473331c9801576f9eaf35862d2
Daniil <65498838+polodarb@users.noreply.github.com>
2023-07-18 22:21:46 +0300

    Deleted Room DB

--- app/src/main/java/com/polodarb/gmsflags/data/repository/Repository.kt
+++ app/src/main/java/com/polodarb/gmsflags/data/repository/Repository.kt
@@ -1,10 +1,9 @@
 package com.polodarb.gmsflags.data.repository
 
-import com.polodarb.gmsflags.data.local.FlagsDAO
 import javax.inject.Inject
 
 class Repository @Inject constructor(
-    val localDB: FlagsDAO
+
 ) {
 
 }
