d53a7048cc006764f36ea71ef29e5b3d91e26280
polodarb <daniil.kobzar78@gmail.com>
2023-09-04 00:36:14 +0300

    Implemented ClickableText in WelcomeScreen

--- app/src/main/java/ua/polodarb/gmsflags/ui/screens/welcomeScreens/WelcomeScreen.kt
+++ app/src/main/java/ua/polodarb/gmsflags/ui/screens/welcomeScreens/WelcomeScreen.kt
@@ -1,6 +1,7 @@
 package ua.polodarb.gmsflags.ui.screens.welcomeScreens
 
 import android.content.res.Configuration
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -41,7 +44,9 @@ fun WelcomeScreen() {
             Image(
                 imageVector = getGMSImagesWelcome(colorScheme = MaterialTheme.colorScheme),
                 contentDescription = "",
-                modifier = Modifier.align(Alignment.CenterHorizontally).padding(top = 16.dp)
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 16.dp)
             )
             Text(
                 text = "Welcome!",
@@ -79,37 +84,62 @@ fun WelcomeScreen() {
                     fontSize = 16.sp
                 )
             }
-            Text(
-                text = buildAnnotatedString {
-                    withStyle(style = SpanStyle(fontWeight = FontWeight.Normal, fontSize = 15.sp)) {
-                        append("By continuing, you agree to our ")
+            val annotatedString = buildAnnotatedString {
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Normal, fontSize = 15.sp)) {
+                    append("By continuing, you agree to our ")
+                }
+
+                pushStringAnnotation(tag = "policy", annotation = "https://google.com/policy")
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 15.sp,
+                        textDecoration = TextDecoration.Underline,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                ) {
+                    append("Privacy Policy")
+                }
+                pop()
+
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Normal, fontSize = 15.sp)) {
+                    append(" and ")
+                }
+
+                pushStringAnnotation(tag = "terms", annotation = "https://google.com/terms")
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 15.sp,
+                        textDecoration = TextDecoration.Underline,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                ) {
+                    append("Terms of Service")
+                }
+                pop()
+            }
+
+            ClickableText(
+                text = annotatedString, style = TextStyle(
+                    textAlign = TextAlign.Center
+                ), onClick = { offset ->
+                    annotatedString.getStringAnnotations(
+                        tag = "policy",
+                        start = offset,
+                        end = offset
+                    ).firstOrNull()?.let {
+                        Log.d("policy URL", it.item)
                     }
-                    withStyle(
-                        style = SpanStyle(
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 15.sp,
-                            textDecoration = TextDecoration.Underline,
-                            color = MaterialTheme.colorScheme.primary
-                        )
-                    ) {
-                        append("Terms of Service")
-                    }
-                    withStyle(style = SpanStyle(fontWeight = FontWeight.Normal, fontSize = 15.sp)) {
-                        append(" and ")
-                    }
-                    withStyle(
-                        style = SpanStyle(
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 15.sp,
-                            textDecoration = TextDecoration.Underline,
-                            color = MaterialTheme.colorScheme.primary
-                        )
-                    ) {
-                        append("Privacy Policy")
+
+                    annotatedString.getStringAnnotations(
+                        tag = "terms",
+                        start = offset,
+                        end = offset
+                    ).firstOrNull()?.let {
+                        Log.d("terms URL", it.item)
                     }
                 },
-                textAlign = TextAlign.Center,
-                lineHeight = 20.sp,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(start = 24.dp, end = 24.dp, bottom = 16.dp)
