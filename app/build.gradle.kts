96ce63f3708ce746b009bd5b1ff402cd0607204d
polodarb <daniil.kobzar78@gmail.com>
2023-09-13 12:56:05 +0300

    Updated versionCode to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "ua.polodarb.gmsflags"
         minSdk = 29
         targetSdk = 33
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
