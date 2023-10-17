e5db9beb4a5877e19ff560faeaa3aaa9c1b63bb3
Danyil Kobzar <65498838+polodarb@users.noreply.github.com>
2023-10-18 01:37:09 +0300

    Fixed clearing phenotype cache fun in SuggestionScreenViewModel

--- app/src/main/java/ua/polodarb/gmsflags/ui/screens/suggestionsScreen/SuggestionScreenViewModel.kt
+++ app/src/main/java/ua/polodarb/gmsflags/ui/screens/suggestionsScreen/SuggestionScreenViewModel.kt
@@ -128,11 +128,26 @@ class SuggestionScreenViewModel(
         return Json.decodeFromString(pkgContent)
     }
 
-    private fun clearPhenotypeCache(pkgName: String) {
-        val androidPkgName = repository.androidPackage(pkgName)
-        CoroutineScope(Dispatchers.IO).launch {
-            Shell.cmd("am force-stop $androidPkgName").exec()
-            Shell.cmd("rm -rf /data/data/$androidPkgName/files/phenotype").exec()
+    fun clearPhenotypeCache(pkgName: String) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val androidPkgName = repository.androidPackage(pkgName)
+                Shell.cmd("am force-stop $androidPkgName").exec()
+                Shell.cmd("rm -rf /data/data/$androidPkgName/files/phenotype").exec()
+                if (pkgName.contains("finsky") || pkgName.contains("vending")) {
+                    Shell.cmd("rm -rf /data/data/com.android.vending/files/experiment*").exec()
+                    Shell.cmd("am force-stop com.android.vending").exec()
+                }
+                if (pkgName.contains("com.google.android.apps.photos")) {
+                    Shell.cmd("rm -rf /data/data/com.google.android.apps.photos/shared_prefs/phenotype*").exec()
+                    Shell.cmd("rm -rf /data/data/com.google.android.apps.photos/shared_prefs/com.google.android.apps.photos.phenotype.xml").exec()
+                    Shell.cmd("am force-stop com.google.android.apps.photos").exec()
+                }
+                repeat(3) {
+                    Shell.cmd("am start -a android.intent.action.MAIN -n $androidPkgName &").exec()
+                    Shell.cmd("am force-stop $androidPkgName").exec()
+                }
+            }
         }
     }
 
