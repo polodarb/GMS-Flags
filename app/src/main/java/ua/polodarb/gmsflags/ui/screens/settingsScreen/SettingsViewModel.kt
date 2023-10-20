e73aba3c81fe7947db33334045bbde288e31808f
Danyil Kobzar <65498838+polodarb@users.noreply.github.com>
2023-10-20 22:30:02 +0300

    Added removed phenotype cache of Google Photos and Play Store in settings

--- app/src/main/java/ua/polodarb/gmsflags/ui/screens/settingsScreen/SettingsViewModel.kt
+++ app/src/main/java/ua/polodarb/gmsflags/ui/screens/settingsScreen/SettingsViewModel.kt
@@ -2,6 +2,7 @@ package ua.polodarb.gmsflags.ui.screens.settingsScreen
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.topjohnwu.superuser.Shell
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -9,20 +10,36 @@ import ua.polodarb.gmsflags.data.repo.SettingsRepository
 
 class SettingsViewModel(
     val settingsRepository: SettingsRepository
-): ViewModel() {
+) : ViewModel() {
 
     // GMS DATABASE
     fun deleteAllOverriddenFlagsFromGMS() {
         settingsRepository.deleteAllOverriddenFlagsFromGMS()
+        clearCache()
     }
 
     fun deleteAllOverriddenFlagsFromPlayStore() {
         settingsRepository.deleteAllOverriddenFlagsFromPlayStore()
+        clearCache()
     }
 
     fun deleteAllOverriddenFlags() {
         settingsRepository.deleteAllOverriddenFlagsFromGMS()
         settingsRepository.deleteAllOverriddenFlagsFromPlayStore()
+        clearCache()
+    }
+
+    // Clear Google Photos and Play Store phenotype cache
+    private fun clearCache() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                Shell.cmd("rm -rf /data/data/com.android.vending/files/experiment*").exec()
+                Shell.cmd("am force-stop com.android.vending").exec()
+                Shell.cmd("rm -rf /data/data/com.google.android.apps.photos/shared_prefs/phenotype*").exec()
+                Shell.cmd("rm -rf /data/data/com.google.android.apps.photos/shared_prefs/com.google.android.apps.photos.phenotype.xml").exec()
+                Shell.cmd("am force-stop com.google.android.apps.photos").exec()
+            }
+        }
     }
 
     // Local Room DATABASE
