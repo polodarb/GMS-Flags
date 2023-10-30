a1eaee847c3ac23c7018a233d64df9c1f378a0e8
Danyil Kobzar <65498838+polodarb@users.noreply.github.com>
2023-10-30 03:03:04 +0200

    Added isShrinkResources to build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
