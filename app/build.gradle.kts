ba1b1e8ec1085ac9b9401e5a9847ede8cdfb0ed4
Danyil Kobzar <65498838+polodarb@users.noreply.github.com>
2023-11-05 22:53:38 +0200

    Updated app version to v1.0.9 (build.gradle)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "ua.polodarb.gmsflags"
         minSdk = 29
         targetSdk = 33
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
