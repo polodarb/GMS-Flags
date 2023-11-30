7990621c4dffb5e84a0f7505ddb396037b2769b6
Danyil Kobzar <65498838+polodarb@users.noreply.github.com>
2023-12-01 00:31:51 +0200

    Fix hang when adding multiple flags

--- app/src/main/java/ua/polodarb/gmsflags/data/repo/interactors/GmsDBInteractor.kt
+++ app/src/main/java/ua/polodarb/gmsflags/data/repo/interactors/GmsDBInteractor.kt
@@ -3,6 +3,7 @@ package ua.polodarb.gmsflags.data.repo.interactors
 import android.content.Context
 import android.util.Log
 import com.topjohnwu.superuser.Shell
+import kotlinx.coroutines.flow.first
 import ua.polodarb.gmsflags.data.repo.GmsDBRepository
 
 class GmsDBInteractor(
@@ -54,7 +55,7 @@ class GmsDBInteractor(
     }
 
     suspend fun clearPhenotypeCache(pkgName: String) {
-        repository.androidPackage(pkgName).collect { androidPkgName ->
+        val androidPkgName = repository.androidPackage(pkgName).first()
             Shell.cmd("am force-stop $androidPkgName").exec()
             Shell.cmd("rm -rf /data/data/$androidPkgName/files/phenotype").exec()
             if (pkgName.contains("finsky") || pkgName.contains("vending")) {
@@ -72,7 +73,6 @@ class GmsDBInteractor(
                 Shell.cmd("am start -a android.intent.action.MAIN -n $androidPkgName &").exec()
                 Shell.cmd("am force-stop $androidPkgName").exec()
             }
-        }
     }
 
 }
